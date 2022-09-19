147566f9ab279db244472eb55722745b4c02574d
Vitor Gabriel <66925616+vgabriel10@users.noreply.github.com>
2022-09-19 11:20:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,36 @@ app.get('/about',(req,res) => {
     })
 })
 
+app.get('/usuarios',(req,res) => {
+    res.json({
+        nome: 'João Ferreira',
+        dataNascimento: '10/12/2000',
+        email:'joão@gmail.com',
+        telefone: '988884142',
+        cidade: 'Fortaleza/CE'
+    }),
+    ({
+        nome: 'Isabel Costa',
+        dataNascimento: '10/12/2000',
+        email:'isabelcost@gmail.com',
+        telefone: '988888442',
+        cidade: 'Fortaleza/CE'
+    }),
+    ({
+        nome: 'Marcelo Sousa',
+        dataNascimento: '10/12/2000',
+        email:'marcelooosousa@gmail.com',
+        telefone: '988544142',
+        cidade: 'Fortaleza/CE'
+    }),
+    ({
+        nome: 'Leticia Silva',
+        dataNascimento: '10/12/2000',
+        email:'leticiaa@gmail.com',
+        telefone: '988784042',
+        cidade: 'Fortaleza/CE'
+    })
+})
 
 app.get('/',(req,res) => {
     res.json({
